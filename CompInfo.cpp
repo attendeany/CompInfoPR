e4c30a6dd58f326c57fc9d7e04509e2ff6485d6b
attendeany <39012750+attendeany@users.noreply.github.com>
2019-02-08 16:50:00 +0300

    Версия 1.2.1
    1) Исправлено обнаружение Win 8.1/10
    2) Добавлено обнаружение сервиспака
    3) Добавлено обнаружение сервисного пакета (может работать нестабильно)
    TODO: Функция не открывает ключи рееста с точкой.

--- CompInfo.cpp
+++ CompInfo.cpp
@@ -11,7 +11,7 @@
 //#include <winsock.h>
 //#pragma comment(lib, "Wsock32.lib")
 #pragma comment(lib, "ws2_32.lib")
-
+ 
 using namespace std;
 
 string get_comp_name()
@@ -56,6 +56,7 @@ struct SysInfo{
 	string hostname;
 	vector<string>ip_list;
 	string win_version;
+	string office_version;
 
 	void export_to_file()
 	{
@@ -66,7 +67,7 @@ struct SysInfo{
 		for (const auto &i : this->ip_list) {
 			fout << i << endl;
 		}
-		fout << this->win_version;
+		fout << this->win_version << endl << this->office_version;
 
 		cout << "saved to " << filename << endl;
 	}
@@ -75,33 +76,36 @@ struct SysInfo{
 void get_win_version(string &win_version)
 {
 	//Version
-	DWORD encodedVersion = GetVersion(),
-		dwmajorVersion,
-		dwminorVersion,
-		dwBuild;
+	NTSTATUS(WINAPI *RtlGetVersion)(LPOSVERSIONINFOEXW);
+	OSVERSIONINFOEXW osInfo;
 
-	dwmajorVersion = (DWORD)(LOBYTE(LOWORD(encodedVersion)));
-	dwminorVersion = (DWORD)(HIBYTE(LOWORD(encodedVersion)));
+	*(FARPROC*)&RtlGetVersion = GetProcAddress(GetModuleHandleA("ntdll"), "RtlGetVersion");
 
-	if (encodedVersion < 0x80000000)
-		dwBuild = (DWORD)(HIWORD(encodedVersion));
-	
-	switch (dwmajorVersion)
+	if (NULL != RtlGetVersion)
+	{
+		osInfo.dwOSVersionInfoSize = sizeof(osInfo);
+		RtlGetVersion(&osInfo);
+	}
+	std::wstring sp(osInfo.szCSDVersion);
+	if (!sp.empty())
+		sp.insert(sp.begin(),' ');
+
+	switch (osInfo.dwMajorVersion)
 	{
 	case 5:
-		if (dwminorVersion == 0)
-			win_version = "Windows 2000 build " + to_string(dwBuild);
-		else if (dwminorVersion == 1)
-				win_version = "Windows XP x32 build " + to_string(dwBuild);
+		if (osInfo.dwMinorVersion == 0)
+			win_version = "Windows 2000 build " + to_string(osInfo.dwBuildNumber);
+		else if (osInfo.dwMinorVersion == 1)
+				win_version = "Windows XP x32 build " + string(sp.begin(), sp.end()) + ' ' + to_string(osInfo.dwBuildNumber);
 			 else
-				win_version = "Windows XP x64 build " + to_string(dwBuild);
+				win_version = "Windows XP x64 build " + string(sp.begin(), sp.end()) + ' ' + to_string(osInfo.dwBuildNumber);
 			 return;
 	case 6:
-		if (dwminorVersion == 0)
+		if (osInfo.dwMinorVersion == 0)
 			win_version = "Windows Vista ";
-		else if (dwminorVersion == 1)
+		else if (osInfo.dwMinorVersion == 1)
 			win_version = "Windows 7 ";
-		else if (dwminorVersion == 2)
+		else if (osInfo.dwMinorVersion == 2)
 			win_version = "Windows 8 ";
 		else
 			win_version = "Windows 8.1 ";
@@ -142,9 +146,81 @@ void get_win_version(string &win_version)
 		bit_deph = "Unknown architecture";
 		break;
 	}
-
-	win_version += bit_deph + " build "+ to_string(dwBuild);
+	win_version += bit_deph + string(sp.begin(),sp.end()) + " build "+ to_string(osInfo.dwBuildNumber);
 	cout << win_version << endl;
+}
+
+void get_office_version(string &office_version)
+{
+	enum OfficeVer {
+		Office97 = 7, Office98 = 8, Office2000 = 9, OfficeXP = 10,
+		Office2003 = 11, Office2007 = 12, Office2010 = 14, Office2013 = 15, Office2016 = 16};
+	try {	
+		HKEY rKey;
+		if (RegOpenKeyA(HKEY_LOCAL_MACHINE, "Software\\Microsoft\\Office", &rKey) != ERROR_SUCCESS)
+			throw(string("Не удалось открыть ключ реестра"));
+
+		char name[512];
+		DWORD boofsize = sizeof(name);
+		int index = 0;
+		while (RegEnumKeyExA(rKey, index++, name, &boofsize, NULL, NULL, NULL, NULL) == ERROR_SUCCESS)
+		{
+			//string path = "Software\\Microsoft\\Office\\" + string(name) + "\\Outlook";
+			//HKEY Office_rKey;
+			//if (RegOpenKeyA(HKEY_LOCAL_MACHINE, path.c_str(), &Office_rKey) != ERROR_SUCCESS)
+			//	continue;
+			
+			/*byte bitness;
+			DWORD boofbitness = sizeof(bitness);
+			if (RegQueryValueExA(Office_rKey, "Bitness",NULL,NULL,&bitness,&boofbitness) != ERROR_SUCCESS) {
+				RegCloseKey(Office_rKey);
+				continue;}*/
+			switch (atoi(name))
+			{
+			case Office2016:
+				office_version = "Microsoft Office 2016";
+				break;
+			case Office2013:
+				office_version = "Microsoft Office 2013";
+				break;
+			case Office2010:
+				office_version = "Microsoft Office 2010";
+				break;
+			case Office2007:
+				office_version = "Microsoft Office 2007";
+				break;
+			case Office2003:
+				office_version = "Microsoft Office 2003";
+				break;
+			case OfficeXP:
+				office_version = "Microsoft Office XP";
+				break;
+			case Office2000:
+				office_version = "Microsoft Office 2000";
+				break;
+			case Office98:
+				office_version = "Microsoft Office 98";
+				break;
+			case Office97:
+				office_version = "Microsoft Office 97";
+				break;
+			default:
+				break;
+			}
+			if (!office_version.empty()) {
+				RegCloseKey(rKey);
+				break;}
+		}
+	}
+	catch (string err)
+	{
+		office_version = "Ошибка чтения реестра";
+		cout << err;
+		return;
+	}
+	if (office_version.empty())
+		office_version = "Microsoft Office отсутствует";
+	cout << office_version << endl;
 }
 
 int main()
@@ -164,6 +240,8 @@ int main()
 	WSACleanup();
 
 	get_win_version(info.win_version);
+
+	get_office_version(info.office_version);
 
 	info.export_to_file();
 
